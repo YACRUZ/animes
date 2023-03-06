6f566bb976d8368253c79504c7f1bf65934cfc00
YACRUZ <yamatsukino@gmail.com>
2023-03-06 13:36:36 -0600

    create mutations

--- caps/schema.py
+++ caps/schema.py
@@ -14,3 +14,53 @@ class Query(graphene.ObjectType):
 
     def resolve_caps(self, info, **kwargs):
         return Cap.objects.all()
+
+#1
+class CreateCap(graphene.Mutation):
+    id = graphene.Int()
+    titulo = graphene.String()
+    temporada = graphene.String()
+    genero = graphene.String()
+    capitulos = graphene.Int()
+    estudio = graphene.String()
+    director = graphene.String()
+    animacion = graphene.String()
+    formato = graphene.String()
+    adaptacion = graphene.String()
+
+    #2
+    class Arguments:
+        titulo = graphene.String()
+        temporada = graphene.String()
+        genero = graphene.String()
+        capitulos = graphene.Int()
+        estudio = graphene.String()
+        director = graphene.String()
+        animacion = graphene.String()
+        formato = graphene.String()
+        adaptacion = graphene.String()
+
+
+    #3
+    def mutate(self, info, titulo, temporada, genero, capitulos, estudio, director, animacion, formato, adaptacion):
+        cap = Cap(titulo=titulo, temporada=temporada, genero=genero, capitulos=capitulos, 
+                   estudio=estudio, director=director, animacion=animacion, formato=formato, adaptacion=adaptacion)
+        cap.save()
+
+        return CreateCap(
+            id=cap.id,
+            titulo=cap.titulo,
+            temporada=cap.temporada,
+            genero=cap.genero,
+            capitulos=cap.capitulos,
+            estudio=cap.estudio,
+            director=cap.director,
+            animacion=cap.animacion,
+            formato=cap.formato,
+            adaptacion=cap.adaptacion
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_cap = CreateCap.Field()
